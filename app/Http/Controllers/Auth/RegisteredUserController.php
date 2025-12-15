6466d219a9d36ed27ea004efb00e6912bb2f909b
Marjan ghazi <syedmarjanghazi@gmail.com>
2025-12-15 15:11:31 +0500

    updated

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,13 +32,15 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'role' => ['required', 'in:student,teacher'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'role' => $request->role, // 👈 role saved
             'password' => Hash::make($request->password),
         ]);
 
