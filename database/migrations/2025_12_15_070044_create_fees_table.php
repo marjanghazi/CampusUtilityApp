7772b43e07259e95183e7ad00cf6e5ee0f949f96
Marjan ghazi <syedmarjanghazi@gmail.com>
2025-12-15 12:01:24 +0500

    updated

--- database/migrations/2025_12_15_070044_create_fees_table.php
+++ database/migrations/2025_12_15_070044_create_fees_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('fees', function (Blueprint $table) {
             $table->id();
+            $table->decimal('amount');
+            $table->string('status');
+            $table->date('due_date');
             $table->timestamps();
         });
     }
